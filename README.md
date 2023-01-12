52b06ed620d82f42d9a1cffbcb3b3b912efb6f7b
o0morgan0o <morgan.thibert@gmail.com>
2023-01-12 01:32:32 +0100

    feat improvements add test_runner

--- README.md
+++ README.md
@@ -7,4 +7,11 @@ If you want to create a linux script with unit testing with bats, you must do so
 ## Usage
 
 Download and execute the script at `./dev/bats-creator.sh`.
+It will ask you the name of your script.
+The script will generate all configuration for utilisation of bats.
+Write your test in `test` folder, by naming new file with `.bats` extension.
+An example of test file is available at `./test/test.bats`
 
+You can also run `run-tests.sh` to run all `.bats` test files in `test` folder.
+
+See the **bats** official documentation [here](https://bats-core.readthedocs.io/en/stable/)
--- dev/bats-creator.sh
+++ dev/bats-creator.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
+
+# ==========================================================
+# This script will create a folder structure to use bats
+#
+# Usage: ./bats-creator.sh
+# Usage: ./bats-creator.sh -- force (will delete existing folders src and test)
+# Morgan Thibert ; January-2023
+# ==========================================================
+
 # we want to exit if any command fails
 set -e
 
-DEV_MODE=false
+# we ensure that we are in the right folder
+SCRIPT_PATH="$(realpath "$0")"
+cd "$(dirname "$SCRIPT_PATH")"
 
+DEV_MODE=false
 # if we enter we --force flag, we will delete everything done by this script
 if [[ $1 == "--force" ]]; then
     DEV_MODE=true
@@ -15,8 +27,13 @@ if [[  $DEV_MODE == "true" ]]; then
     rm -rf ./.git
     rm -rf ./src
     rm -rf ./test
-    # todo exit if src or test folder exist
+    rm -rf ./run-tests.sh
 fi
+
+# we ask the user the name of the script
+echo "What is the name of your script ? (default: my_script.sh)"
+read -r script_name
+script_name=${script_name:-my_script.sh}
 
 if [[ -d ./src ]]; then
     echo "src folder already exist. Please delete it before running this script."
@@ -51,6 +68,17 @@ function _no_git_folder_error() {
     echo "Please init a git repository before using this script."
     _set_output_reset_style
     exit 1
+}
+
+function _install_modules(){
+    _set_output_to_purple
+    echo "Installing required submodules..."
+    _set_output_reset_style
+    git submodule add https://github.com/bats-core/bats-core.git test/bats
+    git submodule add https://github.com/bats-core/bats-support.git test/test_helper/bats-support
+    git submodule add https://github.com/bats-core/bats-assert.git test/test_helper/bats-assert
+    git submodule add https://github.com/buildkite-plugins/bats-mock test/helpers/mocks
+
 }
 
 function _create_common_setup(){
@@ -90,7 +118,7 @@ function _create_test_bats_file(){
         echo ""
         echo "@test \"our first test\" {";
         echo "  # replace with your actual script";
-        echo "  run my_script";
+        echo "  run $script_name";
         echo "  assert_output --partial \"Hello World\"";
         echo "  assert_success";
         echo "}";
@@ -98,21 +126,53 @@ function _create_test_bats_file(){
     } >> $test_bats_file
 }
 
+function _create_test_runner_script(){
+    # creation of file
+    test_runner_script="run-tests.sh"
+    touch $test_runner_script
+    # set contents of example test file
+    {
+        echo "#!/bin/bash";
+    } >> $test_runner_script
+    find ./test -maxdepth 1 -name "*.bats" -type f -exec echo "./test/bats/bin/bats {}" \; >> "$test_runner_script"
+    chmod +x "$test_runner_script"
+}
+
 function _create_example_script_file() {
     # creation of file
-    script_file="src/my_script.sh"
-    touch $script_file
+    script_file="src/$script_name"
+    touch "$script_file"
 
     # set contents of example script file
     {
         echo "#!/bin/bash";
         echo "echo \"Hello World\"";
-    } >> $script_file
+    } >> "$script_file"
 
     # be able to execute the script
-    chmod +x $script_file
+    chmod +x "$script_file"
 }
 
+function _show_usage_banner(){
+    echo ""
+    echo "------------------------------------"
+    echo "Done. You can now run your tests with:"
+    _set_output_to_purple
+    echo "  ./test/bats/bin/bats test/test.bats"
+    _set_output_reset_style
+    echo "------------------------------------"
+    echo "  or (if you have bats installed globally):"
+    _set_output_to_purple
+    echo "  bats test/test.bats"
+    _set_output_reset_style
+    echo "------------------------------------"
+    echo "  run all bats test files:"
+    _set_output_to_purple
+    echo "  ./run-tests.sh"
+    _set_output_reset_style
+    echo "------------------------------------"
+    echo "Happy testing !"
+}
 
 
 # if no .git folder exist, we ask the user if he want us to create it
@@ -133,16 +193,8 @@ fi
 mkdir src
 mkdir test
 
-
 # first we install the required submodules
-_set_output_to_purple
-echo "Installing required submodules..."
-_set_output_reset_style
-git submodule add https://github.com/bats-core/bats-core.git test/bats
-git submodule add https://github.com/bats-core/bats-support.git test/test_helper/bats-support
-git submodule add https://github.com/bats-core/bats-assert.git test/test_helper/bats-assert
-git submodule add https://github.com/buildkite-plugins/bats-mock test/helpers/mocks
-
+_install_modules
 
 # creation of common-setup file in test_helper/common-
 _create_common_setup
@@ -153,19 +205,8 @@ _create_test_bats_file
 # creation of example script file
 _create_example_script_file
 
+# creation of test runner to run all bats tests
+_create_test_runner_script
 
 # all files have been created, now we should be able to run the test
-echo ""
-echo "------------------------------------"
-echo "Done. You can now run your tests with:"
-_set_output_to_purple
-echo "  ./test/bats/bin/bats test/test.bats"
-_set_output_reset_style
-echo "------------------------------------"
-echo "  or (if you have bats installed globally):"
-_set_output_to_purple
-echo "  bats test/test.bats"
-_set_output_reset_style
-echo "------------------------------------"
-echo "Happy testing !"
-
+_show_usage_banner
